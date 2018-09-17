3f12a4efb924303582fe814347886361a9367bc4
bobzyh <bobzyh@163.com>
2018-09-17 17:38:26 +0800

    123

---
+++ src/com/situ/helloworld/HelloWorld.java
@@ -0,0 +1,11 @@
+package com.situ.helloworld;
+
+public class HelloWorld {
+	
+	public static void main(String[] args) {
+		System.out.println("Hello World !");
+		
+		int i = 0;
+		System.out.println(i);
+	}
+}
